5750e18697ad39846bd80e7f5d8f54f1c8a3a159
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-09 23:17:27 +0545

    feat: add User model (src/models/user.model.ts)

---
+++ src/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IUser } from "../types/user.type";
+
+export interface IUserDocument extends Omit<IUser, "_id">, Document {}
+
+const UserSchema = new Schema<IUserDocument>(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model<IUserDocument>("User", UserSchema);
+
+export default UserModel;
