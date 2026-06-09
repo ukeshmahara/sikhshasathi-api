ccb65a98a20cbeb1d4cabb25dfb1d206313037bd
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-09 23:17:15 +0545

    feat: add HTTP exception class (src/exceptions/http-exception.ts)

---
+++ src/exceptions/http-exception.ts
@@ -0,0 +1,10 @@
+export class HttpException extends Error {
+  public status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+    this.name = "HttpException";
+    Object.setPrototypeOf(this, HttpException.prototype);
+  }
+}
